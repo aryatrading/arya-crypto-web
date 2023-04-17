57be49199c9fe6d58f016613e194e5a0eff1edeb
AbdallaEmadEldinAbdalla <aemadeldin@arya.com>
2023-04-17 13:50:26 +0400

    feat: Add assets and change the image dir import way.

--- src/assets/images/images.ts
+++
@@ -1,9 +0,0 @@
-import logo from "./logo.svg";
-import login from "./login.png";
-import logoIcon from "./logo_icon.svg";
-
-export const images = {
-  logo,
-  login,
-  logoIcon,
-};
---
+++ src/assets/images/index.ts
@@ -0,0 +1,11 @@
+import logo from "./logo.svg"
+import apple from './auth/apple.svg';
+import google from './auth/google.svg';
+import logoIcon from './auth/logoWithoutText.svg';
+
+export const images = {
+    logo,
+    apple,
+    google,
+    logoIcon,
+}
