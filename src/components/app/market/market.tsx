be70bacb8ebc61948bf88a330459ab42a9ba5d99
ShareefN <snuseibeh@arya.xyz>
2023-04-17 11:29:33 +0400

    shadow button component

--- src/components/app/market/market.tsx
+++ src/components/app/market/market.tsx
@@ -10,6 +10,7 @@ import { ShadowButton } from "../../shared/buttons/shadow_button";
 
 const Market: FC = () => {
   const [title, setTitle] = useState("Cryptocurrencies");
+  const [tab, setTab] = useState("all");
 
   return (
     <div className="h-full items-center justify-center">
@@ -31,15 +32,43 @@ const Market: FC = () => {
         </Row>
         <Row className="w-2/4 justify-between mt-8 mb-2 align-center">
           <SearchInput onChange={() => console.log("d")} placeholder=":" />
-          <Row className="gap-2">
-            <ShadowButton title="All" />
-            <ShadowButton title="Favorites" />
+          <Row className="gap-0.5">
+            <ShadowButton
+              title="All"
+              onClick={() => setTab("all")}
+              border="rounded-l-md"
+              bgColor={tab === "all" ? "bg-blue_three" : "bg-grey_two"}
+              textColor={tab === "all" ? "text-blue_one" : "text-white"}
+            />
+            <ShadowButton
+              title="Favorites"
+              onClick={() => setTab("favorites")}
+              border="rounded-r-md"
+              bgColor={tab === "all" ? "bg-grey_two" : "bg-blue_three"}
+              textColor={tab === "all" ? "text-white" : "text-blue_one"}
+              iconSvg={
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={0}
+                  stroke="currentColor"
+                  className="w-4 h-4 fill-yellow_one "
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"
+                  />
+                </svg>
+              }
+            />
           </Row>
         </Row>
-        {/* <AssetsTable
+        <AssetsTable
           header={marketAssetsHeader}
           assets={dummyMarket as unknown as AssetType}
-        /> */}
+        />
       </Col>
     </div>
   );
--- src/components/shared/buttons/shadow_button.tsx
+++ src/components/shared/buttons/shadow_button.tsx
@@ -2,15 +2,30 @@ import { FC } from "react";
 
 type ShadowButtonProps = {
   title: string;
-  iconSvg?: FC;
+  iconSvg?: any;
   border?: string;
   bgColor?: string;
+  onClick: Function;
+  textColor: string;
 };
 
-export const ShadowButton: FC<ShadowButtonProps> = ({ title }) => {
+export const ShadowButton: FC<ShadowButtonProps> = ({
+  title,
+  iconSvg,
+  border,
+  bgColor,
+  onClick,
+  textColor,
+}) => {
   return (
-    <div className="bg-red-500">
-      <h1>{title}</h1>
+    <div
+      className={`flex flex-row items-center gap-2 justify-center px-5 py-2.5 ${
+        border ?? ""
+      } ${bgColor} hover:cursor-pointer`}
+      onClick={() => onClick!()}
+    >
+      {iconSvg ?? null}
+      <h1 className={`${textColor} font-semibold text-sm`}>{title}</h1>
     </div>
   );
 };
