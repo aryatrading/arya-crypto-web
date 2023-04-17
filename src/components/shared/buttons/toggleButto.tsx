4af443382b34db725631ddaebd6c33684a2242ed
ShareefN <snuseibeh@arya.xyz>
2023-04-17 10:18:18 +0400

    shadow buttons compoent

---
+++ src/components/shared/buttons/toggleButto.tsx
@@ -0,0 +1,10 @@
+import { FC } from "react";
+
+type ToggleButtonProps = {
+  title: string;
+  iconSvg?: FC;
+};
+
+export const ToggleButton: FC<ToggleButtonProps> = ({}) => {
+  return <h1>ff</h1>;
+};
