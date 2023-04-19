911f33098f424e66e1c0f0f35a775cae4bb6419e
Almujtaba <amohammed@arya.com>
2023-04-19 10:40:16 +0400

    feat: updated tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,24 +9,27 @@ module.exports = {
       sm: '480px',
       md: '768px',
       lg: '976px',
-      xl: '1440px',
+      xl: '1280px',
+      '2xl': '1440px',
     },
-    colors: {
-      green_one: "#22C55E",
-      green_two: "#0E3F2D",
-      blue_two: "#84ADFF",
-      blue_one: "#558AF2",
-      blue_three: "#213660",
-      red_one: "#F2323F",
-      red_two: "#440C10",
-      grey_one: "#6B7280",
-      grey_two: "#111827",
-      grey_three: "#1F2A41",
-      grey_four: "#2B3958",
-      black_two: "#0E1421",
-      black_one: "#030712",
-      yellow_one: "#F3BA2F"
-    }
+    extend: {
+      colors: {
+        green_one: "#22C55E",
+        green_two: "#0E3F2D",
+        blue_two: "#84ADFF",
+        blue_one: "#558AF2",
+        blue_three: "#213660",
+        red_one: "#F2323F",
+        red_two: "#440C10",
+        grey_one: "#6B7280",
+        grey_two: "#111827",
+        grey_three: "#1F2A41",
+        grey_four: "#2B3958",
+        black_two: "#0E1421",
+        black_one: "#030712",
+        yellow_one: "#F3BA2F"
+      }
+    },
   },
   plugins: [],
 }
