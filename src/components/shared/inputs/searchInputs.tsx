50ca1c8b888a475047b00598d46a9f26e8713565
ShareefN <snuseibeh@arya.xyz>
2023-04-25 11:22:01 +0400

    prop optimization

--- src/components/shared/inputs/searchInputs.tsx
+++ src/components/shared/inputs/searchInputs.tsx
@@ -1,11 +1,14 @@
 import { FC } from "react";
 
 type SearchInputProps = {
-  onChange: Function;
+  onchange: Function;
   placeholder: string;
 };
 
-export const SearchInput: FC<SearchInputProps> = ({}) => {
+export const SearchInput: FC<SearchInputProps> = ({
+  onchange,
+  placeholder,
+}) => {
   return (
     <form className="flex items-center w-1/3">
       <label className="sr-only">Search</label>
@@ -29,8 +32,9 @@ export const SearchInput: FC<SearchInputProps> = ({}) => {
           type="text"
           id="simple-search"
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          placeholder="Search"
+          placeholder={placeholder}
           required
+          onChange={(e) => onchange(e.target.value)}
         />
       </div>
     </form>
