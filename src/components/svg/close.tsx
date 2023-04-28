289a92c83cf2adae0ff45409cb78a90681f08fd7
AbdallaEmadEldinAbdalla <aemadeldin@arya.com>
2023-04-28 15:33:10 +0400

    feat: Add 2 svg icons.

---
+++ src/components/svg/close.tsx
@@ -0,0 +1,6 @@
+export const CloseIcon = () => (
+    <svg width="8" height="8" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M13 13L2 2" stroke="#D6D6D6" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round" />
+        <path d="M13 2L2 13" stroke="#D6D6D6" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round" />
+    </svg>
+);
