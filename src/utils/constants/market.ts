4440c1ad0a41744bae33c1511a994e01814d4fc3
Almujtaba <amohammed@arya.com>
2023-05-01 11:58:35 +0400

    feat: graph data ranges

---
+++ src/utils/constants/market.ts
@@ -0,0 +1,9 @@
+import { GraphDataRange } from "../../components/shared/charts/graph/graph.type";
+
+export const AllowedGraphDataRanges: GraphDataRange[] = [
+    GraphDataRange["24h"],
+    GraphDataRange["week"],
+    GraphDataRange["month"],
+    GraphDataRange["six_month"],
+    GraphDataRange["year"],
+]
