05ff31f9476f47c50aca40da89a8e07f81cf5e85
AbdallaEmadEldinAbdalla <aemadeldin@arya.com>
2023-05-01 17:26:25 +0400

    fix: Fix translation issue.

--- src/components/shared/coinProfitCalculator/index.tsx
+++ src/components/shared/coinProfitCalculator/index.tsx
@@ -1,4 +1,4 @@
-import { useTranslation } from "react-i18next";
+import { useTranslation } from "next-i18next";
 import { useCallback, useEffect, useMemo, useState } from "react";
 import clsx from "clsx";
 
