49e9478bf4afb2673d9dff0646a4243f68efcd9a
ShareefN <snuseibeh@arya.xyz>
2023-05-01 12:02:05 +0400

    paylaod mapping change

--- src/services/controllers/asset.tsx
+++ src/services/controllers/asset.tsx
@@ -1,30 +1,33 @@
 import { AssetType } from "../../types/asset";
+import { CapitalizeString } from "../../utils/format_string";
 import { dispatchAction } from "../../utils/global_dispatch";
 import { axiosInstance } from "../api/axiosConfig";
 import { setAsset } from "../redux/assetSlice";
 
 export const getAssetDetails = async (symbol?: any) => {
   let { data } = await axiosInstance.get(
-    `/utils/asset-details?asset_name=${symbol}`
+    `trade-engine/exposure-details?asset_name=${symbol}&provider=1`
   );
+
+  let _res = data.asset_details;
 
   let _asset: AssetType = {};
 
-  _asset.iconUrl = data.asset_data.image;
-  _asset.id = data.id;
-  _asset.rank = data.asset_data.market_cap_rank;
-  _asset.currentPrice = data.asset_data.current_price;
-  _asset.symbol = data.asset_data.symbol.toLowerCase();
-  _asset.pnl = data.asset_data.price_change_percentage_24h.toFixed(2);
-  _asset.mrkCap = data.asset_data.market_cap;
-  _asset.volume = data.asset_data.total_volume;
-  _asset.supply = data.asset_data.total_supply;
-  _asset.dailyLow = data.asset_data.low_24h;
-  _asset.dailyHigh = data.asset_data.high_24h;
-  _asset.dilutedValuation = data.asset_data.fully_diluted_valuation;
-  _asset.name = data.asset_data.name;
-  _asset.priceChange = data.asset_data.price_change_24h.toFixed(3);
-  _asset.circlSupply = data.asset_data.circulating_supply;
+  _asset.iconUrl = _res.image;
+  _asset.id = data.asset_id;
+  _asset.rank = _res.market_cap_rank;
+  _asset.currentPrice = _res.current_price;
+  _asset.symbol = _res.symbol.toLowerCase();
+  _asset.pnl = _res.price_change_percentage_24h.toFixed(2);
+  _asset.mrkCap = _res.market_cap;
+  _asset.volume = _res.total_volume;
+  _asset.supply = _res.total_supply;
+  _asset.dailyLow = _res.low_24h;
+  _asset.dailyHigh = _res.high_24h;
+  _asset.dilutedValuation = _res.fully_diluted_valuation;
+  _asset.name = CapitalizeString(_res.id);
+  _asset.priceChange = _res.price_change_24h.toFixed(3);
+  _asset.circlSupply = _res.circulating_supply;
 
   dispatchAction(setAsset(_asset));
 };
