904612c14323cff07c6637e959625b09b6b09a57
AbdallaEmadEldinAbdalla <aemadeldin@arya.com>
2023-05-05 10:48:22 +0400

    feat: Set max date for the date input to be today.

--- src/components/shared/form/inputs/textInput/index.tsx
+++ src/components/shared/form/inputs/textInput/index.tsx
@@ -8,7 +8,7 @@ import "./input.module.scss";
 export const TextInput: FC<{ label: string, type: string, name: string, placeholder?: string, disabled?: boolean }> = ({ label, ...props }) => (
     <Col className="gap-2">
         {label && <label className="block text-sm dark:text-white font-semibold">{label}</label>}
-        <Field className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" {...props} />
+        <Field max={new Date().toISOString().split('T')[0]} className="text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500" {...props} />
     </Col>
 );
 
