665e77ac6503229acd88990da09e9b23ba48fd8b
AbdallaEmadEldinAbdalla <aemadeldin@arya.com>
2023-05-05 14:40:15 +0400

    enhanc: Revert layout styling.

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Poppins } from "next/font/google";
-import clsx from "clsx";
 
 import Nav from "./Nav/Nav";
 import Footer from "./Footer/Footer";
@@ -10,12 +9,11 @@ const poppins = Poppins({
   weight: ["400", "500", "600"],
   subsets: ["latin"],
 });
-// flex flex-col flex-1 container sm:items-center lg:pt-28 p-5
 export default function Layout({ children }: any) {
   return (
     <div className={poppins.className}>
       <Nav />
-      <div className={clsx('h-full w-full container mx-auto sm:items-center', poppins.className)}>
+      <div className="flex flex-col flex-1 container sm:items-center pt-28">
         {children}
       </div>
       <Footer />
