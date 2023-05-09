db919780c68d119b8ab9698abfca22bf753dac84
Almujtaba <amohammed@arya.com>
2023-05-09 10:09:14 +0400

    feat: removed assets selector button from dashboard

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -11,7 +11,6 @@ const HomePage = (props: any) => {
     return (
         <Layout>
             <Dashboard />
-            <AssetSelector onClick={(asset) => false} trigger={<Button className='px-4 py-2 rounded bg-yellow-1 text-white'>selectAsset</Button>} />
         </Layout>
     )
 }
