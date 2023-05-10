26b520d3287f7d87c57bc2f78dc967cc5dedb4e8
AbdallaEmadEldinAbdalla <aemadeldin@arya.com>
2023-05-10 17:51:57 +0400

    feat: Add the postTypes.

--- src/types/asset.tsx
+++ src/types/asset.tsx
@@ -25,3 +25,105 @@ export interface AssetType {
   circlSupply?: number;
   isHoldingAsset?: boolean;
 }
+
+
+export interface PostTypes {
+  Category?: string,
+  LastComment?: {
+    Author?: {
+      Id?: string,
+      VanityAlias?: string,
+      PictureDate?: string,
+      Job?: string,
+      Name?: string,
+      SystemAlias?: string
+    },
+    Text?: string,
+    Id?: string,
+    TextLocalizations?: {
+      fr?: string,
+      en?: string
+    },
+    Date?: string,
+    TextLang?: string
+  },
+  Images?: string[],
+  Video?: {
+    size?: number,
+    videoUrl?: string,
+    width?: number,
+    height: number,
+    thumbnailUrl: string
+  },
+  IndexMentions?: [
+    {
+      Length?: number,
+      Start?: number,
+      MarketItem?: {
+        Symbol?: string,
+        To?: string,
+        Market?: string,
+        Key?: string,
+        Name?: string
+      },
+      Name?: string
+    }
+  ],
+  CommentsCount?: number,
+  Author?: {
+    Id?: string,
+    PictureDate?: string,
+    Job?: string,
+    Name?: string
+  },
+  Text?: string,
+  TextLocalizations?: {
+    fr?: string,
+    en?: string
+  },
+  Date?: string,
+  LastActivity?: string,
+  TextLang?: string,
+  _id?: string,
+  FinancialData?: {
+    IsPriceEvaluationCompleted?: boolean,
+    Type?: string,
+    BreakEven?: number,
+    Timeframe?: string,
+    ExpirationDate?: string,
+    Sell?: boolean,
+    Instrument?: {
+      Length?: number,
+      Start?: number,
+      MarketItemKey?: string,
+      MarketItem?: {
+        Symbol?: string,
+        To?: string,
+        Market?: string,
+        Key?: string,
+        Name?: string
+      },
+      Name?: string
+    },
+    StopLoss?: number,
+    EntryValue?: number,
+    PriceEvaluations?: [
+      {
+        ReachedAt?: string,
+        Type?: string,
+        Price?: number
+      },
+    ],
+    TakeProfits?: [
+      {
+        Value?: number
+      }
+    ]
+  },
+  AwardsCount?: any,
+  ReactionCounts?: {
+    Likes?: number,
+  },
+  SharesCount?: number,
+  Audio?: string,
+}
