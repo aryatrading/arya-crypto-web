2bc0d479427e11437a918cda767488f94a50fbd6
AbdallaEmadEldinAbdalla <aemadeldin@arya.com>
2023-05-10 17:50:20 +0400

    feat: Add svg icons.

---
+++ src/components/svg/arrowDown.tsx
@@ -0,0 +1,6 @@
+export const ArrowDown = () => (
+    <svg width="9" height="9" viewBox="0 0 9 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M1 0.999959L7.50004 7.5" stroke="#B1454F" stroke-width="1.4" stroke-linecap="round" stroke-linejoin="round" />
+        <path d="M3.13037 7.5H7.50015V3.13022" stroke="#B1454F" stroke-width="1.4" stroke-linecap="round" stroke-linejoin="round" />
+    </svg>
+);
---
+++ src/components/svg/arrowUp.tsx
@@ -0,0 +1,6 @@
+export const ArrowUp = () => (
+    <svg width="9" height="9" viewBox="0 0 9 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M1.38184 8.22227L8.14066 1" stroke="#22C55E" stroke-width="1.4" stroke-linecap="round" stroke-linejoin="round" />
+        <path d="M3.59766 1H8.1414V5.85531" stroke="#22C55E" stroke-width="1.4" stroke-linecap="round" stroke-linejoin="round" />
+    </svg>
+);
