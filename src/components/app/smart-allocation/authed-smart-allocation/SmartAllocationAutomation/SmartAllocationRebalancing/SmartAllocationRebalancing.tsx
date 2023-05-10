6be95b546bf09c68b609dfdc782a7101db73aa5a
Ameer <akhan@arya.com>
2023-05-10 09:11:42 +0400

    Translation Added

--- src/components/app/smart-allocation/authed-smart-allocation/SmartAllocationAutomation/SmartAllocationRebalancing/SmartAllocationRebalancing.tsx
+++ src/components/app/smart-allocation/authed-smart-allocation/SmartAllocationAutomation/SmartAllocationRebalancing/SmartAllocationRebalancing.tsx
@@ -14,6 +14,7 @@ import * as AlertDialog from '@radix-ui/react-alert-dialog'
 import * as Checkbox from '@radix-ui/react-checkbox'
 import { toast } from 'react-toastify'
 import { CheckIcon, TrashIcon } from '@heroicons/react/24/outline'
+import { useTranslation } from 'next-i18next'
 
 const SmartAllocationRebalancing = () => {
     const {rebalancingDate,rebalancingFrequency,getSmartAllocationData} = useContext(SmartAllocationContext)
@@ -21,6 +22,7 @@ const SmartAllocationRebalancing = () => {
     const selectedExchange = useSelector(selectSelectedExchange);
     const [rebalanceNow, setRebalanceNow] = useState<boolean|'indeterminate'>(false)
     const [rebalanceModalOpen, setRebalanceModalOpen] = useState<boolean>(false)
+    const {t} = useTranslation(['smart-allocation','common'])
 
     const saveRebalancingFrequency = useCallback(
         (frequency?:EnumRebalancingFrequency|null) => {
@@ -62,23 +64,23 @@ const SmartAllocationRebalancing = () => {
                   <AlertDialog.Portal>
                       <AlertDialog.Overlay className="dialog-overlay data-[state=open]:animate-overlayShow" />
                       <AlertDialog.Content className="dialog-content data-[state=open]:animate-contentShow">
-                          <AlertDialog.Title className="dialog-title">Delete Rebalancing Frequency?</AlertDialog.Title>
+                          <AlertDialog.Title className="dialog-title">{t('deleteFrequencyTitle')}</AlertDialog.Title>
                           <AlertDialog.Description className="dialog-description">
-                              This will delete the rebalancing frequency and your portfolio wont rebalance as scheduled. Are you sure you want to continue?
+                              {t('deleteFrequencyDescription')}
                           </AlertDialog.Description>
                           <div className='flex flex-col gap-6 justify-end text-base font-semibold'>
                             <AlertDialog.Action asChild>
-                                <Button className="px-14 py-2  bg-red-1 rounded-lg" onClick={()=>saveRebalancingFrequency(null)}>Delete Frequency</Button>
+                                <Button className="px-14 py-2  bg-red-1 rounded-lg" onClick={()=>saveRebalancingFrequency(null)}>{t('deleteFrequency')}</Button>
                             </AlertDialog.Action>
                             <AlertDialog.Cancel asChild>
-                                <Button className="alert-dialog-cancel-btn">Cancel</Button>
+                                <Button className="alert-dialog-cancel-btn">{t('common:cancel')}</Button>
                             </AlertDialog.Cancel>
                           </div>
                       </AlertDialog.Content>
                   </AlertDialog.Portal>
               </AlertDialog.Root>
         },
-        [saveRebalancingFrequency],
+        [saveRebalancingFrequency, t],
       )
 
 
@@ -86,15 +88,15 @@ const SmartAllocationRebalancing = () => {
       () => {
         return <AlertDialog.Root open={rebalanceModalOpen} onOpenChange={setRebalanceModalOpen} >
                 <AlertDialog.Trigger asChild>
-                    <Button className='bg-blue-1 w-56 py-3 rounded-lg font-semibold text-xs'>Set Rebalancing frequency</Button>
+                    <Button className='bg-blue-1 w-56 py-3 rounded-lg font-semibold text-xs'>{t('setRebalancingFrequency')}</Button>
                 </AlertDialog.Trigger>
                 <AlertDialog.Portal>
                     <AlertDialog.Overlay className="dialog-overlay data-[state=open]:animate-overlayShow" />
                     <AlertDialog.Content className="dialog-content data-[state=open]:animate-contentShow gap-8">
                         <div className='flex flex-col items-center gap-4'>
-                            <AlertDialog.Title className="dialog-title">Update Rebalancing Frequency?</AlertDialog.Title>
+                            <AlertDialog.Title className="dialog-title">{t('updateFrequencyTitle')}</AlertDialog.Title>
                             <AlertDialog.Description className="dialog-description">
-                                This will update the rebalancing frequency and change the next rebalancing date. Are you sure you want to continue?
+                                {t('updateFrequencyDescription')}
                             </AlertDialog.Description>
                             <div className="flex items-center gap-4">
                                 <Checkbox.Root
@@ -108,23 +110,23 @@ const SmartAllocationRebalancing = () => {
                                     </Checkbox.Indicator>
                                 </Checkbox.Root>
                                 <label className="text-base leading-none text-white font-medium" htmlFor="c1">
-                                    Rebalance Now
+                                    {t('rebalanceNow')}
                                 </label>
                             </div>
                         </div>
                         <div className='flex flex-col gap-6 items-center text-base font-semibold'>
                             <AlertDialog.Action asChild>
-                                <Button className=" bg-blue-1 rounded-lg py-2 px-14" onClick={()=>saveRebalancingFrequency()}>Update Frequency</Button>
+                                <Button className=" bg-blue-1 rounded-lg py-2 px-14" onClick={()=>saveRebalancingFrequency()}>{t('updateFrequency')}</Button>
                             </AlertDialog.Action>
                             <AlertDialog.Cancel asChild>
-                                <Button className="alert-dialog-cancel-btn">Cancel</Button>
+                                <Button className="alert-dialog-cancel-btn">{t('common:cancel')}</Button>
                             </AlertDialog.Cancel>
                         </div>
                     </AlertDialog.Content>
                 </AlertDialog.Portal>
             </AlertDialog.Root>
       },
-      [rebalanceModalOpen, rebalanceNow, saveRebalancingFrequency],
+      [rebalanceModalOpen, rebalanceNow, saveRebalancingFrequency, t],
     )
     
       useEffect(()=>{
@@ -136,29 +138,29 @@ const SmartAllocationRebalancing = () => {
         <Col className='items-start gap-7'>
             <Row className='flex py-5 px-4 bg-black-2 rounded-lg w-full justify-between lg:w-1/2'>
                 <Col className='gap-2 text-sm'>
-                    <span className='font-semibold text-white'>Current Rebalancing Frequency</span>
+                    <span className='font-semibold text-white'>{t('currentRebalancingFrequency')}</span>
                     {rebalancingFrequency?
-                        <span className='text-grey-1'>Rebalancing is set to <span className='text-blue-1'>{`${CapitalizeString(rebalancingFrequency??'')}, next rebalancing is at ${moment(rebalancingDate).calendar()}`}</span></span>
+                        <span className='text-grey-1'>{t('RebalancingDetail')} <span className='text-blue-1'>{`${t(`common:${rebalancingFrequency}`)}, ${t('NextRebalancingAt')} ${moment(rebalancingDate).calendar()}`}</span></span>
                         :
-                        <span className='text-grey-1'>Rebalancing is not set</span>
+                        <span className='text-grey-1'>{t('noRebalancingFrequencySet')}</span>
                     }
                 </Col>
                 {!!rebalancingFrequency&&deleteRebalancingFrequency()}
             </Row>
             <Col className='text-sm gap-3 w-full lg:w-2/3'>
-                <span className='font-semibold text-white'>What is Rebalancing Frequency</span>
-                <span className='text-grey-1'>Rebalancing frequency is the frequency at which your portfolio will be rebalanced. Rebalancing is the process of realigning the weightings of a portfolio of assets. Rebalancing involves periodically buying or selling assets in a portfolio to maintain an original or desired level of asset allocation or risk.</span>
+                <span className='font-semibold text-white'>{t('WhatIsRebalancingFrequency')}</span>
+                <span className='text-grey-1'>{t('RebalancingFrequencyDescription')}</span>
             </Col>
             <Col className='gap-5 w-full'>
-                <span className='font-semibold'>Choose a time frequency</span>
+                <span className='font-semibold'>{t('chooseFrequency')}</span>
                 <div className='flex gap-3'>
                     <Button className= {twMerge('rounded-lg bg-black-2 py-4 w-full lg:w-56 text-xs font-semibold text-grey-1',(tempFrequency===null)?'bg-blue-3 text-blue-1':'')} onClick={() => setTempFrequency(null)}>
-                        None
+                        {t('common:none')}
                     </Button>
                     {Object.values(EnumRebalancingFrequency).map((frequency) => {
                         return (
                             <Button className= {twMerge('rounded-lg bg-black-2 py-4 w-full lg:w-56 text-xs font-semibold text-grey-1',(tempFrequency===frequency)?'bg-blue-3 text-blue-1':'')} onClick={() => setTempFrequency(frequency)}>
-                                {CapitalizeString(frequency)}
+                                {t(`common:${frequency}`)}
                             </Button>
                         )
                     })}
