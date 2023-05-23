37e28a9c9342b4e9db2a2bf71b95b185fc89d2d0
AbdallaEmadEldinAbdalla <aemadeldin@arya.com>
2023-05-23 17:38:08 +0400

    fix: Fix auth modal.

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,9 +1,5 @@
 importScripts("https://www.gstatic.com/firebasejs/8.2.0/firebase-app.js");
 importScripts("https://www.gstatic.com/firebasejs/8.2.0/firebase-messaging.js");
-self.addEventListener('fetch', () => {
-    const urlParams = new URLSearchParams(location.search);
-    self.firebaseConfigUrl = Object.fromEntries(urlParams);
-})
 
 // "Default" Firebase configuration (prevents errors)
 const defaultConfig = {
@@ -15,17 +11,3 @@ const defaultConfig = {
 
 
 firebase.initializeApp(self.firebaseConfigUrl || defaultConfig);
-
-const messaging = firebase.messaging();
-messaging.onBackgroundMessage(function (payload) {
-    const notificationTitle = payload.notification.title;
-    const notificationOptions = {
-        body: payload.notification.body,
-        icon: "https://uploads-ssl.webflow.com/62133fadfe3e62071a2d063e/6214aed8ff94c926b744afc9_Group%2012856.png",
-    };
-    return self.registration.showNotification(
-        notificationTitle,
-        notificationOptions
-    );
-});
-
