110874afe34854dd0055aba61958497a0f5fbf1c
AbdallaEmadEldinAbdalla <aemadeldin@arya.com>
2023-05-23 17:22:50 +0400

    fix: Fix the importScript urls.

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts("https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js");
-importScripts("https://www.gstatic.com/firebasejs/9.22.0/firebase-messaging.js");
+importScripts("https://www.gstatic.com/firebasejs/8.2.0/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/8.2.0/firebase-messaging.js");
 
 // "Default" Firebase configuration (prevents errors)
 const defaultConfig = {
