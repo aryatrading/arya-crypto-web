750943f554ae737856e0cc985889e4b38f70398c
AbdallaEmadEldinAbdalla <aemadeldin@arya.com>
2023-05-23 17:24:23 +0400

    fix

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,5 +1,9 @@
 importScripts("https://www.gstatic.com/firebasejs/8.2.0/firebase-app.js");
 importScripts("https://www.gstatic.com/firebasejs/8.2.0/firebase-messaging.js");
+self.addEventListener('fetch', () => {
+    const urlParams = new URLSearchParams(location.search);
+    self.firebaseConfigUrl = Object.fromEntries(urlParams);
+})
 
 // "Default" Firebase configuration (prevents errors)
 const defaultConfig = {
@@ -11,4 +15,17 @@ const defaultConfig = {
 
 
 firebase.initializeApp(self.firebaseConfigUrl || defaultConfig);
+
+const messaging = firebase.messaging();
+messaging.onBackgroundMessage(function (payload) {
+    const notificationTitle = payload.notification.title;
+    const notificationOptions = {
+        body: payload.notification.body,
+        icon: "https://uploads-ssl.webflow.com/62133fadfe3e62071a2d063e/6214aed8ff94c926b744afc9_Group%2012856.png",
+    };
+    return self.registration.showNotification(
+        notificationTitle,
+        notificationOptions
+    );
+});
 
