093cec49f88059611c31c435e8163e7b4530a24e
AbdallaEmadEldinAbdalla <aemadeldin@arya.com>
2023-05-26 14:48:55 +0400

    enhanc: Modify the `Add exchange` to work on mobile view.

--- src/components/app/exchangeTab/AddExchange.tsx
+++ src/components/app/exchangeTab/AddExchange.tsx
@@ -10,6 +10,7 @@ import { selectAllProviders } from '../../../services/redux/exchangeSlice';
 import clsx from 'clsx';
 import ExchangeImage from '../../shared/exchange-image/exchange-image';
 import { ExchangesIcon } from '../../svg/exchanges';
+import { useResponsive } from '../../../context/responsive.context';
 
 interface AddExchangeTypes {
     onPressExchange: any,
@@ -19,6 +20,7 @@ interface AddExchangeTypes {
 const AddExchange = (props: AddExchangeTypes) => {
     const { t } = useTranslation(['settings', 'common']);
     const allProviders = useSelector(selectAllProviders);
+    const { isTabletOrMobileScreen } = useResponsive();
 
     useEffect(() => {
         getAddableProviders();
@@ -28,17 +30,17 @@ const AddExchange = (props: AddExchangeTypes) => {
 
     const gridView = useMemo(() => {
         return (
-            <Col className={clsx({ "grid-cols-3": (allProviders?.length || 0) % 2 === 1, "grid-cols-2": (allProviders?.length || 0) % 2 === 0 }, "grid gap-4")}>
+            <Col className={clsx({ "grid-cols-3": (allProviders?.length || 0) % 2 === 1, "grid-cols-2": (allProviders?.length || 0) % 2 === 0, "grid-cols-1": isTabletOrMobileScreen }, "grid gap-4")}>
                 {
                     allProviders?.map(provider => {
                         return (
                             <Button className={clsx({ "hover:bg-grey-4": !provider?.isConnected, "cursor-not-allowed": provider?.isConnected }, 'w-[214px] h-[48px] bg-grey-3 rounded-md')} onClick={() => props.onPressExchange((st: any) => [{ provider_id: provider.id, name: provider.name, create: true }, ...st,])} disabled={provider?.isConnected}>
-                                <Row className={clsx({ "justify-between px-4": provider?.isConnected }, 'justify-center items-center')}>
+                                <Row className='px-4 justify-between items-center'>
                                     <Row className='justify-center items-center gap-2'>
                                         <ExchangeImage providerId={provider.id} />
                                         <label className='font-bold text-base text-center text-white'>{provider.name.charAt(0) + provider.name.slice(1).toLowerCase()}</label>
                                     </Row>
-                                    {provider?.isConnected && <CheckBadgeIcon stroke="#22C55E" className="w-6 h-6" />}
+                                    {provider?.isConnected && <CheckBadgeIcon stroke={"#22C55E"} className="w-6 h-6" />}
 
                                 </Row>
                             </Button>
@@ -47,7 +49,7 @@ const AddExchange = (props: AddExchangeTypes) => {
                 }
             </Col>
         );
-    }, [allProviders, props]);
+    }, [allProviders, isTabletOrMobileScreen, props]);
 
     return (
         <Col className={classes}>
