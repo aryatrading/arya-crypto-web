051bf7c6b3890239f1cf2bcc832b005e906df3c5
AbdallaEmadEldinAbdalla <aemadeldin@arya.com>
2023-05-30 15:29:55 +0400

    feat: Add `BTC Dominance` to market screen.

--- src/components/app/market/market.tsx
+++ src/components/app/market/market.tsx
@@ -1,23 +1,49 @@
-import { FC, useCallback, useState } from "react";
+import { FC, useCallback, useEffect, useMemo, useState } from "react";
+import React from "react";
+import { useTranslation } from "next-i18next";
+import { StarIcon } from "@heroicons/react/24/outline";
+import { useSelector } from "react-redux";
+import clsx from "clsx";
+
+
 import { Col, Row } from "../../shared/layout/flex";
 import MarketStats from "../../shared/containers/marketStats";
 import { AssetsTable } from "../../shared/tables/assetsTable";
 import { SearchInput } from "../../shared/inputs/searchInputs";
 import { ShadowButton } from "../../shared/buttons/shadow_button";
-import { StarIcon } from "@heroicons/react/24/outline";
-import { useSelector } from "react-redux";
 import { selectMarketAssets } from "../../../services/redux/marketSlice";
-import { fetchAssets } from "../../../services/controllers/market";
+import { fetchAssets, getMarketCap } from "../../../services/controllers/market";
 import { FAVORITES_LIST } from "../../../utils/constants/config";
 import useDebounce from "../../../utils/useDebounce";
-import React from "react";
-import { useTranslation } from "next-i18next";
 
 const Market: FC = () => {
   const { t } = useTranslation(["market"]);
   const [tab, setTab] = useState("all");
   const [search, setSearch] = useState("");
   const _assets = useSelector(selectMarketAssets);
+  const [marketCapDetails, setMarketCapDetails] = useState<any>({});
+
+  useEffect(() => {
+    getMarketCap().then(({ data: { data: { quote: { USD } } } }) => {
+      setMarketCapDetails({
+        vol24: USD.total_volume_24h,
+        vol24Percentage: USD.total_volume_24h_yesterday_percentage_change,
+        marketCap: USD.total_market_cap,
+        marketCapPercentage: USD.total_market_cap_yesterday_percentage_change,
+        marketCapYesterday: USD.total_market_cap_yesterday
+      })
+    })
+  }, []);
+
+  const BTCDominance = useMemo(() => {
+    const btcMC = _assets.filter((asset: any) => asset.symbol === 'btc')[0]?.mrkCap;
+    return btcMC != null && marketCapDetails?.marketCap != null ? (btcMC / marketCapDetails.marketCap) * 100 : 0;
+  }, [_assets, marketCapDetails]);
+
+  const BTCDominanceYesterday = useMemo(() => {
+    const btcMC = _assets.filter((asset: any) => asset.symbol === 'btc')[0]?.mrkCapYesterday;
+    return btcMC != null && marketCapDetails?.marketCapYesterday != null ? (btcMC / marketCapDetails.marketCapYesterday) * 100 : 0;
+  }, [_assets, marketCapDetails]);
 
   useDebounce(
     () => {
@@ -46,16 +72,24 @@ const Market: FC = () => {
           </p>
           {tab === "all" ? (
             <div className="w-full items-center justify-center gap-4 flex flex-col lg:flex-row">
-              {/* TODO: update hardcoded color values */}
               <MarketStats
-                bgColor="bg-green-2 w-full lg:w-64"
-                value={8.15}
+                bgColor={clsx({ "bg-green-2": marketCapDetails?.marketCapPercentage > 0, "bg-red-2": marketCapDetails?.marketCapPercentage < 0, "bg-grey-2": marketCapDetails?.marketCapPercentage === 0 }, "w-full lg:w-64")}
+                value={marketCapDetails?.marketCapPercentage || '0'}
+                amount={marketCapDetails?.marketCap || '0'}
                 title={t("marketcap")}
               />
               <MarketStats
-                bgColor="bg-red-2 w-full lg:w-64"
-                value={-3.75}
+                bgColor={clsx({ "bg-green-2": marketCapDetails?.vol24Percentage > 0, "bg-red-2": marketCapDetails?.vol24Percentage < 0, "bg-grey-2": marketCapDetails?.vol24Percentage === 0 }, "w-full lg:w-64")}
+                value={marketCapDetails?.vol24Percentage || '0'}
+                amount={marketCapDetails?.vol24 || '0'}
                 title={t("volume")}
+              />
+              <MarketStats
+                bgColor={clsx({ "bg-green-2": BTCDominance > 0, "bg-red-2": BTCDominance < 0, "bg-grey-2": BTCDominance === 0 }, "w-full lg:w-64")}
+                value={BTCDominanceYesterday}
+                amount={String(BTCDominance)}
+                title={t("btcDominance")}
+                percent
               />
             </div>
           ) : null}
--- src/components/shared/containers/marketStats.tsx
+++ src/components/shared/containers/marketStats.tsx
@@ -1,28 +1,33 @@
 import { FC } from "react";
 import { Col, Row } from "../layout/flex";
 import AssetPnl from "./asset/assetPnl";
+import { formatNumber } from "../../../utils/helpers/prices";
 
 type MarketProps = {
   bgColor: string;
   value: number;
   title: string;
+  amount: string;
+  percent?: boolean;
 };
 
 export const MarketStats: FC<MarketProps> = ({
   bgColor,
   value,
   title,
+  amount,
+  percent
 }) => {
   return (
     <Col className={`ml-3.5 mr-3.5 ${bgColor} w-60 rounded-md h-20 pl-5 pt-2`}>
-      <Row>
+      <Row className="items-center">
         <p className="text-[#F9FAFB] text-sm font-medium leading-5 items-center">
           {title}
         </p>
-        <AssetPnl className={(value<0)?'text-red-1':'text-green-1'} value={value} />
+        <AssetPnl className={(value < 0) ? 'text-red-1' : 'text-green-1'} value={value} />
       </Row>
       <p className="text-[#F9FAFB] text-xl font-medium leading-7 pt-2">
-        $1,227,747,623,904
+        {percent ? '' : '$'}{formatNumber(parseFloat(amount))}{percent && '%'}
       </p>
     </Col>
   );
