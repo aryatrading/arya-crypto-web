ce5184fb69635466c9e64091ac756052012ce700
AbdallaEmadEldinAbdalla <aemadeldin@arya.com>
2023-05-31 11:44:10 +0400

    fix: Fix btcDominance issue.

--- src/components/shared/containers/marketStats.tsx
+++ src/components/shared/containers/marketStats.tsx
@@ -22,7 +22,7 @@ export const MarketStats: FC<MarketProps> = ({
         <p className="text-[#F9FAFB] text-sm font-medium leading-5 items-center max-w-[100px]">
           {title}
         </p>
-        <AssetPnl className={(value < 0) ? 'text-red-1' : 'text-green-1'} value={value} />
+        <AssetPnl className={(percent < 0) ? 'text-red-1' : 'text-green-1'} value={percent} />
       </Row>
       <p className="text-[#F9FAFB] text-xl font-medium leading-7 pt-2">
         {percent ? '' : '$'}{formatNumber(parseFloat(amount))}{percent && '%'}
