071c846a817b372269c1fad2252d041dea892335
Almujtaba <amohammed@arya.com>
2023-06-09 08:58:17 +0400

    feat: dimming assets colors by 10%

--- src/utils/helpers/coinsColors.ts
+++ src/utils/helpers/coinsColors.ts
@@ -1,10 +1,37 @@
 import { chartDefaultColorsHex, coinsColors } from "../constants/customColors";
 
+
+function shadeColor(color: string, percent: number) {
+
+    let R = parseInt(color.substring(1, 3), 16);
+    let G = parseInt(color.substring(3, 5), 16);
+    let B = parseInt(color.substring(5, 7), 16);
+
+    R = parseInt(`${R * (100 + percent) / 100}`);
+    G = parseInt(`${G * (100 + percent) / 100}`);
+    B = parseInt(`${B * (100 + percent) / 100}`);
+
+    R = (R < 255) ? R : 255;
+    G = (G < 255) ? G : 255;
+    B = (B < 255) ? B : 255;
+
+    R = Math.round(R)
+    G = Math.round(G)
+    B = Math.round(B)
+
+    let RR = ((R.toString(16).length === 1) ? "0" + R.toString(16) : R.toString(16));
+    let GG = ((G.toString(16).length === 1) ? "0" + G.toString(16) : G.toString(16));
+    let BB = ((B.toString(16).length === 1) ? "0" + B.toString(16) : B.toString(16));
+
+    return "#" + RR + GG + BB;
+}
+
 export function getCoinColor(symbol: string, defaultColorIndex: number) {
+    const shadePercent = -10;
     const defaultColor = chartDefaultColorsHex[defaultColorIndex % chartDefaultColorsHex.length];
     if (symbol) {
-        return coinsColors[symbol?.toLowerCase()] ?? defaultColor;
+        return shadeColor(coinsColors[symbol?.toLowerCase()] ?? defaultColor, shadePercent);
     } else {
-        return defaultColor;
+        return shadeColor(defaultColor, shadePercent);
     }
 }
