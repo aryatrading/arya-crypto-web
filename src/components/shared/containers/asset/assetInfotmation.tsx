f7a60dfb7c2a7c2fdc9435586dfc0640d8d05d74
Almujtaba <amohammed@arya.com>
2023-06-13 17:18:10 +0400

    feat: added asset info loading skeleton

--- src/components/shared/containers/asset/assetInfotmation.tsx
+++ src/components/shared/containers/asset/assetInfotmation.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, useMemo } from "react";
 import { Col } from "../../layout/flex";
 import { AssetType } from "../../../../types/asset";
 import { useSelector } from "react-redux";
@@ -7,6 +7,7 @@ import { useTranslation } from "next-i18next";
 import { formatNumber } from "../../../../utils/helpers/prices";
 import styles from './assetInformation.module.scss'
 import { twMerge } from "tailwind-merge";
+import { TextSkeleton } from "../../skeletons/skeletons";
 
 type AssetInformationProps = {
   asset: AssetType;
@@ -15,37 +16,63 @@ type AssetInformationProps = {
 export const AssetInformation: FC<AssetInformationProps> = ({ asset }) => {
   const { t } = useTranslation(["asset"]);
   const _assetprice = useSelector(selectAssetLivePrice);
+
+  const infoLoadingSkeleton = useMemo(() => {
+    return (
+      <Col className="gap-4">
+        <Col className="gap-2">
+          <TextSkeleton widthClassName="w-40" />
+          <TextSkeleton widthClassName="w-full" />
+          <TextSkeleton widthClassName="w-full" />
+        </Col>
+        <Col className="gap-2">
+          <TextSkeleton widthClassName="w-40" />
+          <TextSkeleton widthClassName="w-full" />
+          <TextSkeleton widthClassName="w-full" />
+          <TextSkeleton widthClassName="w-full" />
+          <TextSkeleton widthClassName="w-full" />
+          <TextSkeleton widthClassName="w-full" />
+          <TextSkeleton/>
+        </Col>
+      </Col>
+    )
+  }, [])
+
   return (
     <Col className="gap-11">
-      <Col className="gap-4">
-        <h2 className="asset-header">{t("pricelivedata", { asset })}</h2>
-        <p className="font-medium text-xs md:text-sm">
-          <strong>
-            {t("pricetday", { asset })}
-            {formatNumber(
-              _assetprice[asset.symbol?.toLowerCase() ?? ""] ??
-                asset.currentPrice,
-              true
-            )}
-          </strong>
-          {asset.pnl <= 0 ? t("drop") : t("increase")}
-          {asset.pnl}% {t("suply", { asset })}
-          {formatNumber(asset.volume ?? 0)} {t("lasttwentyfour")}
-          <br />
-          {t("cap", { asset })}
-          {formatNumber(asset.mrkCap ?? 0)} {t("circsuply")}{" "}
-          {formatNumber(asset.circlSupply ?? 0)} {asset.symbol?.toUpperCase()}{" "}
-          {t("totalsupplyof")} {formatNumber(asset.supply ?? 0)}{" "}
-          {asset.symbol?.toUpperCase()}.
-        </p>
-      </Col>
-      {asset?.description&&
+      {asset.name ?
         <Col className="gap-4">
-        <p className="font-medium text-base md:text-xl">
-          {t("whatis")} {asset.name}
-        </p>
-        <p className={twMerge("font-semibold text-xs md:text-sm",styles.description)}>{ asset?.description.replace(/<\/?[^>]+(>|$)/g, "")}</p>
-      </Col>}
+          <h2 className="asset-header">{t("pricelivedata", { asset })}</h2>
+          <p className="font-medium text-xs md:text-sm">
+            <strong>
+              {t("pricetday", { asset })}
+              {formatNumber(
+                _assetprice[asset.symbol?.toLowerCase() ?? ""] ??
+                asset.currentPrice,
+                true
+              )}
+            </strong>
+            {asset.pnl <= 0 ? t("drop") : t("increase")}
+            {asset.pnl}% {t("suply", { asset })}
+            {formatNumber(asset.volume ?? 0)} {t("lasttwentyfour")}
+            <br />
+            {t("cap", { asset })}
+            {formatNumber(asset.mrkCap ?? 0)} {t("circsuply")}{" "}
+            {formatNumber(asset.circlSupply ?? 0)} {asset.symbol?.toUpperCase()}{" "}
+            {t("totalsupplyof")} {formatNumber(asset.supply ?? 0)}{" "}
+            {asset.symbol?.toUpperCase()}.
+          </p>
+        </Col>
+        : infoLoadingSkeleton
+      }
+
+      {asset?.description &&
+        <Col className="gap-4">
+          <p className="font-medium text-base md:text-xl">
+            {t("whatis")} {asset.name}
+          </p>
+          <p className={twMerge("font-semibold text-xs md:text-sm", styles.description)}>{asset?.description.replace(/<\/?[^>]+(>|$)/g, "")}</p>
+        </Col>}
     </Col>
   );
 };
