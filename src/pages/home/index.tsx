a7cadd135018dbe85f8daaa96ac3cc54cccaa3c8
AbdallaEmadEldinAbdalla <aemadeldin@arya.com>
2023-06-14 16:27:54 +0400

    enhanc: Enhance the home sales page and make it aligned with remaining sales pages.

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -70,111 +70,112 @@ const HomePage = () => {
     return (
         <SalesPagesLayout>
             <SEO />
-            <Row className={clsx("gap-10 flex-col-reverse lg:flex-row items-center px-4 md:px-12 lg:px-20 xl:px-60 py-20 justify-center min-h-[calc(100vh-65px)]", styles.bgImage)}>
-                <Col className='flex-1 w-full h-full gap-4 justify-center'>
+            <Col className="w-full h-full pt-14 lg:pt-0">
+                <Row className={clsx("gap-10 flex-col-reverse lg:flex-row items-center px-4 md:px-12 lg:px-20 xl:px-60 py-20 justify-center min-h-[calc(100vh-65px)]", styles.mainBanner)}>
+                    <Col className='flex-1 w-full h-full gap-4 justify-center'>
+                        <p className='top-0 left-0 text-left w-full text-2xl font-bold md:leading-snug md:text-5xl'>
+                            <Trans i18nKey={'home:title'}
+                                components={{ blueText: <span className='text-blue-1' />, curveText: <span className={styles.underlined} /> }}
+                            /></p>
 
-                    <p className='top-0 left-0 text-left w-full text-2xl font-bold md:leading-snug md:text-5xl'>
-                        <Trans i18nKey={'home:title'}
-                            components={{ blueText: <span className='text-blue-1' />, curveText: <span className={styles.underlined} /> }}
-                        /></p>
+                        <p className='font-medium text-base'>
+                            {t('description')}
+                        </p>
 
-                    <p className='font-medium text-base'>
-                        {t('description')}
-                    </p>
+                        <Col className='gap-4'>
+                            {[t('points.1'), t('points.2'), t('points.3')].map(e => {
+                                return (
+                                    <RowItem content={e} />
+                                );
+                            })}
+                        </Col>
 
-                    <Col className='gap-4'>
-                        {[t('points.1'), t('points.2'), t('points.3')].map(e => {
-                            return (
-                                <RowItem content={e} />
-                            );
-                        })}
+                        <Button className={clsx('h-[44px] bg-blue-1 rounded-lg max-w-[220px] font-bold mt-4 mb-10', styles.boxShadow)}>
+                            {t('tryForFree')}
+                        </Button>
                     </Col>
 
-                    <Button className={clsx('h-[44px] bg-blue-1 rounded-lg max-w-[220px] font-bold mt-4 mb-10', styles.boxShadow)}>
-                        {t('tryForFree')}
-                    </Button>
-                </Col>
-
-                <Col className="flex-1 w-full h-full gap-4 relative">
-                    <Lottie animationData={LottieData} lottieRef={lottieRef} className={clsx(styles.animateImg, "w-full absolute scale-[1.3] h-full")} />
-                    <Lottie animationData={LottieData} className={clsx("w-full opacity-0 h-full")} />
-                </Col>
-            </Row>
-
-            <Col className='bg-black-2 py-10'>
-                <Row className='items-center justify-center gap-10 flex-col-reverse lg:flex-row mb-20 px-4 md:px-14 lg:px-20 xl:px-80 pt-20'>
-                    <Col className="flex-1 w-full gap-4 justify-center">
-                        <h2 className="font-bold text-white text-3xl md:text-5xl text-left leading-snug">{t('secondSection.1.title')}</h2>
-                        <h2 className="font-bold text-white text-3xl md:text-2xl text-left leading-snug">{t('secondSection.1.sub')}</h2>
-                        <p className="font-medium text-white text-left">{t('secondSection.1.content')}</p>
-
-                        <button className={clsx(styles.startBtn, styles.boxShadow, "text-white font-bold text-sm w-fit rounded-full")}>
-                            {t('learnMore')}
-                        </button>
-                    </Col>
-
-                    <Col className='flex-1 relative justify-center items-center'>
-                        <PortfolioImg />
+                    <Col className="flex-1 w-full h-full gap-4 relative">
+                        <Lottie animationData={LottieData} lottieRef={lottieRef} className={clsx(styles.animateImg, "w-full absolute scale-[1.35] lg:scale-150 xl:scale-[1.35] h-full")} />
+                        <Lottie animationData={LottieData} className={clsx("w-full opacity-0 h-full")} />
                     </Col>
                 </Row>
 
-                <Row className='items-center justify-center gap-10 flex-col lg:flex-row mb-20 px-4 md:px-14 lg:px-20 xl:px-80 pt-20'>
-                    <Col className='flex-1 relative justify-center items-center'>
-                        <SmartAllocationImg />
-                    </Col>
+                <Col className='bg-black-2 py-10'>
+                    <Row className='items-center justify-center gap-10 flex-col-reverse lg:flex-row mb-20 px-4 md:px-14 lg:px-20 xl:px-80 pt-20'>
+                        <Col className="flex-1 w-full gap-4 justify-center">
+                            <h2 className="font-bold text-white text-3xl md:text-5xl text-left leading-snug">{t('secondSection.1.title')}</h2>
+                            <h2 className="font-bold text-white text-3xl md:text-2xl text-left leading-snug">{t('secondSection.1.sub')}</h2>
+                            <p className="font-medium text-white text-left">{t('secondSection.1.content')}</p>
 
-                    <Col className="flex-1 w-full gap-4 justify-center">
-                        <h2 className="font-bold text-white text-3xl md:text-5xl text-left leading-snug">{t('secondSection.2.title')}</h2>
-                        <h2 className="font-bold text-white text-3xl md:text-2xl text-left leading-snug">{t('secondSection.2.sub')}</h2>
-                        <p className="font-medium text-white text-left">{t('secondSection.2.content')}</p>
+                            <button className={clsx(styles.startBtn, styles.boxShadow, "text-white font-bold text-sm w-fit rounded-full")}>
+                                {t('learnMore')}
+                            </button>
+                        </Col>
 
-                        <button className={clsx(styles.startBtn, styles.boxShadow, "text-white font-bold text-sm w-fit rounded-full")}>
-                            {t('learnMore')}
-                        </button>
-                    </Col>
-                </Row>
+                        <Col className='flex-1 relative justify-center items-center'>
+                            <PortfolioImg />
+                        </Col>
+                    </Row>
 
-                <Row className='items-center justify-center gap-10 flex-col-reverse lg:flex-row mb-20 px-4 md:px-14 lg:px-20 xl:px-80 pt-20'>
-                    <Col className="flex-1 w-full gap-4 justify-center">
-                        <h2 className="font-bold text-white text-3xl md:text-5xl text-left leading-snug">{t('secondSection.3.title')}</h2>
-                        <h2 className="font-bold text-white text-3xl md:text-2xl text-left leading-snug">{t('secondSection.3.sub')}</h2>
-                        <p className="font-medium text-white text-left">{t('secondSection.3.content')}</p>
+                    <Row className='items-center justify-center gap-10 flex-col lg:flex-row mb-20 px-4 md:px-14 lg:px-20 xl:px-80 pt-20'>
+                        <Col className='flex-1 relative justify-center items-center'>
+                            <SmartAllocationImg />
+                        </Col>
 
-                        <button className={clsx(styles.startBtn, styles.boxShadow, "text-white font-bold text-sm w-fit rounded-full")}>
-                            {t('learnMore')}
-                        </button>
-                    </Col>
+                        <Col className="flex-1 w-full gap-4 justify-center">
+                            <h2 className="font-bold text-white text-3xl md:text-5xl text-left leading-snug">{t('secondSection.2.title')}</h2>
+                            <h2 className="font-bold text-white text-3xl md:text-2xl text-left leading-snug">{t('secondSection.2.sub')}</h2>
+                            <p className="font-medium text-white text-left">{t('secondSection.2.content')}</p>
 
-                    <Col className='flex-1 relative justify-center items-center'>
-                        <TradingImg />
-                    </Col>
-                </Row>
-            </Col>
+                            <button className={clsx(styles.startBtn, styles.boxShadow, "text-white font-bold text-sm w-fit rounded-full")}>
+                                {t('learnMore')}
+                            </button>
+                        </Col>
+                    </Row>
 
-            <PricingSection />
-            <Testimonials />
+                    <Row className='items-center justify-center gap-10 flex-col-reverse lg:flex-row mb-20 px-4 md:px-14 lg:px-20 xl:px-80 pt-20'>
+                        <Col className="flex-1 w-full gap-4 justify-center">
+                            <h2 className="font-bold text-white text-3xl md:text-5xl text-left leading-snug">{t('secondSection.3.title')}</h2>
+                            <h2 className="font-bold text-white text-3xl md:text-2xl text-left leading-snug">{t('secondSection.3.sub')}</h2>
+                            <p className="font-medium text-white text-left">{t('secondSection.3.content')}</p>
 
-            <Col className='py-10 gap-6 items-center justify-center'>
-                <h2 className="font-bold text-white text-3xl md:text-5xl text-left leading-snug mb-10">{t('faq')}</h2>
+                            <button className={clsx(styles.startBtn, styles.boxShadow, "text-white font-bold text-sm w-fit rounded-full")}>
+                                {t('learnMore')}
+                            </button>
+                        </Col>
 
-                <Col className='w-full min-h-20 border border-blue-2 p-6 rounded-lg gap-4 md:max-w-[80%]'>
-                    <p className='font-bold text-white text-lg'>{t('questions.1.Q')}</p>
-                    <p className='font-medium text-white text-base'>{t('questions.1.A')}</p>
+                        <Col className='flex-1 relative justify-center items-center'>
+                            <TradingImg />
+                        </Col>
+                    </Row>
                 </Col>
 
-                <Col className='w-full min-h-20 border border-blue-2 p-6 rounded-lg gap-4 md:max-w-[80%]'>
-                    <p className='font-bold text-white text-lg'>{t('questions.2.Q')}</p>
-                    <p className='font-medium text-white text-base'>{t('questions.2.A')}</p>
-                </Col>
+                <PricingSection />
+                <Testimonials />
 
-                <Col className='w-full min-h-20 border border-blue-2 p-6 rounded-lg gap-4 md:max-w-[80%]'>
-                    <p className='font-bold text-white text-lg'>{t('questions.3.Q')}</p>
-                    <p className='font-medium text-white text-base'>{t('questions.3.A')}</p>
-                </Col>
+                <Col className='py-10 gap-6 items-center justify-center'>
+                    <h2 className="font-bold text-white text-3xl md:text-5xl text-left leading-snug mb-10">{t('faq')}</h2>
 
-                <Col className='w-full min-h-20 border border-blue-2 p-6 rounded-lg gap-4 md:max-w-[80%]'>
-                    <p className='font-bold text-white text-lg'>{t('questions.4.Q')}</p>
-                    <p className='font-medium text-white text-base'>{t('questions.4.A')}</p>
+                    <Col className='w-full min-h-20 border border-blue-2 p-6 rounded-lg gap-4 md:max-w-[80%]'>
+                        <p className='font-bold text-white text-lg'>{t('questions.1.Q')}</p>
+                        <p className='font-medium text-white text-base'>{t('questions.1.A')}</p>
+                    </Col>
+
+                    <Col className='w-full min-h-20 border border-blue-2 p-6 rounded-lg gap-4 md:max-w-[80%]'>
+                        <p className='font-bold text-white text-lg'>{t('questions.2.Q')}</p>
+                        <p className='font-medium text-white text-base'>{t('questions.2.A')}</p>
+                    </Col>
+
+                    <Col className='w-full min-h-20 border border-blue-2 p-6 rounded-lg gap-4 md:max-w-[80%]'>
+                        <p className='font-bold text-white text-lg'>{t('questions.3.Q')}</p>
+                        <p className='font-medium text-white text-base'>{t('questions.3.A')}</p>
+                    </Col>
+
+                    <Col className='w-full min-h-20 border border-blue-2 p-6 rounded-lg gap-4 md:max-w-[80%]'>
+                        <p className='font-bold text-white text-lg'>{t('questions.4.Q')}</p>
+                        <p className='font-medium text-white text-base'>{t('questions.4.A')}</p>
+                    </Col>
                 </Col>
             </Col>
 
