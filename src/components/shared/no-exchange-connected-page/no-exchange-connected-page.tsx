2c51d3145fa8f513bbeeba5259c70608603496b7
AbdallaEmadEldinAbdalla <aemadeldin@arya.com>
2023-07-05 12:20:00 +0400

    fix: Fix comments.

--- src/components/shared/no-exchange-connected-page/no-exchange-connected-page.tsx
+++ src/components/shared/no-exchange-connected-page/no-exchange-connected-page.tsx
@@ -16,7 +16,7 @@ const NoConnectedExchangePage = ({ Component }: { Component?: any }) => {
 
   return (
     <Col className="relative w-full h-screen items-center justify-center">
-      <Col className="gap-5 items-center justify-center z-40 bg-black-2 px-14 rounded-lg pb-32">
+      <Col className="gap-5 items-center justify-center z-40 bg-black-2 px-14 rounded-lg pb-32 mb-40">
         <AddExchange
           onPressExchange={(data: any) => {
             setExchange(data);
@@ -25,7 +25,7 @@ const NoConnectedExchangePage = ({ Component }: { Component?: any }) => {
       </Col>
       {Component &&
         <Col className="blur-md absolute w-full mb-32">
-          <Col className="absolute top-0 left-0 right-0 bottom-0" />
+          <Col className="absolute top-0 left-0 right-0 bottom-0 z-[100]" />
           <Component dummy />
         </Col>
       }
